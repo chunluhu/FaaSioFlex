5d00609fc15a8b78c25245266a5db26c14b1ca91
FaaSioFlex <faasioflex@gmail.com>
2024-10-24 00:47:41 +0200

    Update main.go

--- src/api_faas/main.go
+++ src/api_faas/main.go
@@ -61,7 +61,6 @@ func ejecutarFuncion(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/registrar", registrarFuncion)
 	http.HandleFunc("/ejecutar", ejecutarFuncion)
-
 	
 	fmt.Println("Servidor escuchando en :5000 . Prueba 1")
 	http.ListenAndServe(":5000", nil)
