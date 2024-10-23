0d078e3ea9445f55b10d01a6fd8bc9f47c94f427
FaaSioFlex <faasioflex@gmail.com>
2024-10-23 18:26:54 +0200

    Create main.go

---
+++ src/hello-world/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintln(w, "Hola Mundo")
+}
+
+func main() {
+    http.HandleFunc("/", helloWorld) // Configura la ruta
+    fmt.Println("Servidor escuchando en http://localhost:8080")
+    http.ListenAndServe(":8080", nil) // Inicia el servidor
+}
